c202e9c2968fa7e5895d45f70f67f1579c47fde7
ling <971039649@qq.com>
2021-12-17 10:28:34 +0800

    swagger文档配置

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,36 @@
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { Controller, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
-
+@ApiTags('公共接口')
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: '获取列表信息' })
   @Get('list')
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @ApiOperation({ summary: '更新列表信息' })
   @Post('list')
   create(): string {
     return 'list-post';
   }
 
+  @ApiOperation({ summary: '获取用户信息' })
   @Get('user_*')
   getUser() {
     return 'getUser';
   }
 
+  @ApiOperation({ summary: '更新用户信息' })
   @Put('list/:id')
   update() {
     return 'update';
   }
 
+  @ApiOperation({ summary: '更新用户' })
   @Put('list/user')
   updateUser() {
     return { userID: 1 };
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,3 +1,5 @@
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { CreatePostDto, CreatePostDtoID } from './dto/create-post.dot';
 import { PostsService, PostsRo } from './posts.service';
 import {
   Body,
@@ -10,6 +12,7 @@ import {
   Query,
 } from '@nestjs/common';
 
+@ApiTags('文章')
 @Controller('post')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
@@ -18,14 +21,16 @@ export class PostsController {
    * 创建文章
    * @param post
    */
+  @ApiOperation({ summary: '创建文章' })
   @Post()
-  async create(@Body() post) {
+  async create(@Body() post: CreatePostDto) {
     return await this.postsService.create(post);
   }
 
   /**
    * 获取所有文章
    */
+  @ApiOperation({ summary: '获取所有文章' })
   @Get()
   async findAll(@Query() query): Promise<PostsRo> {
     return await this.postsService.findAll(query);
@@ -35,8 +40,9 @@ export class PostsController {
    * 获取指定文章
    * @param id
    */
+  @ApiOperation({ summary: '根据ID获取文章详情' })
   @Get(':id')
-  async findById(@Param('id') id) {
+  async findById(@Param('id') id: CreatePostDtoID) {
     return await this.postsService.findById(id);
   }
 
@@ -45,8 +51,9 @@ export class PostsController {
    * @param id
    * @param post
    */
+  @ApiOperation({ summary: '更新文章信息' })
   @Put(':id')
-  async update(@Param('id') id, @Body() post) {
+  async update(@Param('id') id: CreatePostDtoID, @Body() post: CreatePostDto) {
     return await this.postsService.updateById(id, post);
   }
 
@@ -54,8 +61,9 @@ export class PostsController {
    * 删除
    * @param id
    */
+  @ApiOperation({ summary: '根据ID删除指定文章' })
   @Delete(':id')
-  async remove(@Param('id') id) {
+  async remove(@Param('id') id: CreatePostDtoID) {
     return await this.postsService.remove(id);
   }
 }
